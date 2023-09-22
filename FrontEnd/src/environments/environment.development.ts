74b002db7f65c2eaeff87077e68d96d6ad1d447c
“nishidaps” <chinchoos11@gmail.com>
2023-09-22 16:54:57 +0530

    environments

--- FrontEnd/src/environments/environment.development.ts
+++ FrontEnd/src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  // baseUrl: 'http://192.168.1.250:8000'
-  baseUrl: 'http://localhost:8000'
+  baseUrl: 'http://192.168.1.250:8000'
+  // baseUrl: 'http://localhost:8000'
 };
